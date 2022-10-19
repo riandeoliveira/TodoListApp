8febd28532dc0b7949de9eb10ad28a71cfd2e472
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-10-19 15:22:03 -0300

    feat: create FilterModal component

---
+++ client/src/components/FilterModal.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <span class="text">Filtrar tarefas por:</span>
+    <ul class="list">
+      <li class="item">Mais antigas</li>
+      <li class="item">Mais recentes</li>
+      <li class="item">Ordem alfabética</li>
+      <li class="item">Concluídas</li>
+      <li class="item">Pendentes</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  background-color: #303030;
+  border-radius: 10px;
+  position: fixed;
+  right: 16px;
+  width: 60%;
+  border: 1px solid #1976d2;
+}
+
+.text {
+  font-size: 14px;
+  font-weight: 500;
+  text-align: center;
+  padding: 10px;
+}
+
+.list {
+  padding-bottom: 10px;
+}
+
+.item {
+  cursor: pointer;
+  padding: 10px;
+}
+</style>
--- client/src/pages/TodosPage.vue
+++ client/src/pages/TodosPage.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
     <HeaderArea />
+    <main class="main">
+      <FilterModal />
+    </main>
     <FooterArea />
   </div>
 </template>
@@ -8,10 +11,16 @@
 <script setup lang="ts">
 import HeaderArea from 'components/HeaderArea.vue';
 import FooterArea from 'src/components/FooterArea.vue';
+import FilterModal from 'src/components/FilterModal.vue';
 </script>
 
 <style lang="scss">
 body {
   background-color: #101010;
+}
+
+.main {
+  padding: 56px 16px;
+  color: white;
 }
 </style>
