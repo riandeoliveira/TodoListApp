ad349ad3edec9ebf6b3a7ececbfb7512b8d75019
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-10-19 13:41:07 -0300

    feat: create IconBase component

---
+++ client/src/components/IconBase.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path :d="name" fill="#ffffff" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name: string;
+}>();
+</script>
