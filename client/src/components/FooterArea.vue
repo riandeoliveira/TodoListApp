4734b20bcaeef47c3c7e339758fc5ea0ba9f4156
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-10-21 15:39:07 -0300

    feat: update FooterArea component

--- client/src/components/FooterArea.vue
+++ client/src/components/FooterArea.vue
@@ -2,8 +2,15 @@
   <footer class="footer">
     <nav class="nav">
       <q-btn flat round title="Adicione uma nova tarefa" class="button">
-        <IconBase :name="add_todo" :size="24" />
+        <IconBase :name="add_todo" :size="24" @click="handleAddTodoField" />
       </q-btn>
+      <q-input
+        v-model="todoName"
+        dark
+        class="input"
+        v-if="readyToAddTodo"
+        ref="inputTodoName"
+      />
     </nav>
   </footer>
 </template>
@@ -11,6 +18,19 @@
 <script setup lang="ts">
 import IconBase from 'components/IconBase.vue';
 import { add_todo } from '../data/icons.json';
+import { Ref, ref } from 'vue';
+
+const readyToAddTodo: Ref<boolean> = ref(false);
+const todoName: Ref<string> = ref('');
+const inputTodoName: Ref<HTMLInputElement | null> = ref(null);
+
+const handleAddTodoField = (): void => {
+  readyToAddTodo.value = !readyToAddTodo.value;
+
+  setTimeout(() => {
+    inputTodoName.value?.focus();
+  }, 0);
+};
 </script>
 
 <style scoped lang="scss">
@@ -20,6 +40,8 @@ import { add_todo } from '../data/icons.json';
   bottom: 0;
   left: 0;
   height: 40px;
+  display: flex;
+  justify-content: center;
   right: 0;
 }
 
@@ -27,12 +49,18 @@ import { add_todo } from '../data/icons.json';
   width: inherit;
   height: inherit;
   display: flex;
+  width: 316px;
   align-items: center;
-  justify-content: space-around;
+  justify-content: center;
+  gap: 8px;
 }
 
 .button {
   min-height: 2.5em;
   min-width: 2.5em;
+}
+
+.input {
+  width: 100%;
 }
 </style>
